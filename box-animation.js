dffa7e91a8f7398e9173f2f6a75c1e53b1af6dde
croantie <melkiibrat91zoomer91>
2023-04-24 21:18:35 +0600

    fixes

--- box-animation.js
+++
@@ -1,8 +0,0 @@
-const box = document.getElementById('box');
-
-box.addEventListener('click', () => {
-    box.classList.add('animation');
-    // setTimeout(() => {
-    //     box.classList.remove('animation')
-    // }, 3000)
-})
--- winners-generator.js
+++ winners-generator.js
@@ -21,33 +21,11 @@ const shuffle = (arr) => {
     winners = arr.slice(0, 14);
 }
 
-// const randomizer = (arr) => {
-//     return Math.floor(Math.random() * participantsList.length)
-// }
-//
-// const findWinner = () => {
-//     box.classList.add('animation');
-//     let winner = randomizer()
-//     winnerBlock.innerHTML = winner + ' ' + participantsList[winner].name
-//     const someWinner = document.createElement('li')
-//     winnersResult.forEach(el => {
-//         if (winner.length > 0 && winner !== el.id) {
-//             console.log(winner)
-//             winnersResult.push(participantsList[winner])
-//             someWinner.innerHTML = el.id + ' ' + el.name
-//             winnersList.appendChild(someWinner)
-//         }
-//     })
-//     setTimeout(() => {
-//         box.classList.remove('animation')
-//     }, 3000)
-// }
-
 const fillWinnersList = () => {
     box.classList.add('animation');
     box.classList.add('disabled');
     let currentPlayer = winners.shift();
-    winnerBlock.innerHTML = currentPlayer.id + ' ' + currentPlayer.name
+    winnerBlock.innerHTML = currentPlayer.name
     winnersResult.push(currentPlayer)
     const someWinner = document.createElement('li')
     someWinner.innerHTML = currentPlayer.id + ' ' + currentPlayer.name
